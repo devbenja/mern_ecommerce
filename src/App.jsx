503d04c1c496aae933b55aa7605ed7cd359643d9
Benjamin Carías <cariasbenjamin59@gmail.com>
2024-05-21 02:07:40 -0600

    Add NavBar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import NavBar from "./components/Navigation/NavBar"
+
+
+export const App = () => {
+  return (
+    <>
+      <NavBar/>
+      <div className="text-xl mt-7 ml-5">Inicio del Ecommerce</div>
+    </>
+    
+  )
+}
+
