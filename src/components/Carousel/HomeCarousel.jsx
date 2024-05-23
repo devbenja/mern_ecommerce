3dac1822431219bff3e1e85d63221535fb0814f0
Benjamin Carías <cariasbenjamin59@gmail.com>
2024-05-23 02:28:19 -0600

    Fix Hero Responsive Design

--- src/components/Carousel/HomeCarousel.jsx
+++ src/components/Carousel/HomeCarousel.jsx
@@ -3,25 +3,19 @@ import 'react-alice-carousel/lib/alice-carousel.css';
 import { HomeCarouselData } from './HomeCarouselData';
 
 const items = HomeCarouselData.map((item) =>
-     <img 
-        src={item.image} 
+    <img
+        src={item.image}
         role='presentation'
-
+        className='img-carousel'
     />
 );
 
 export const HomeCarousel = () => (
-    <div className="relative h-64 lg:h-full">
-        <AliceCarousel
-            items={items}
-            autoPlayInterval={2000}
-            disableButtonsControls
-            autoPlay
-            infinite
-        />
-        <div className="absolute top-1/2 left-10 lg:left-40 transform -translate-y-1/2 p-4">
-            <h2 className='text-sm lg:text-3xl font-bold text-justify'>Explore, Find, Enjoy: Your Virtual Marketplace</h2>
-            <button className="mt-4 bg-indigo-600 text-white py-1 px-6 rounded">Start Exploring</button>
-        </div>
-    </div>
+    <AliceCarousel
+        items={items}
+        autoPlayInterval={2000}
+        disableButtonsControls
+        autoPlay
+        infinite
+    />
 );
--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,9 +1,12 @@
-import { HomeCarousel } from "../../components/Carousel/HomeCarousel.jsx"
+
 
 export const HomePage = () => {
   return (
-    <div className="h-full">
-        <HomeCarousel/>
+    <div className="relative hero-section">
+        <div className="absolute top-1/2 left-10 lg:left-40 transform -translate-y-1/2 p-4">
+            <h2 className='text-xl lg:text-3xl font-bold'>Explore, Find, Enjoy: Your Virtual Marketplace</h2>
+            <button className="mt-4 font-semibold bg-indigo-600 text-white py-1 px-6 rounded">Start Exploring</button>
+        </div>
     </div>
   )
 }
