251cec7bbfb93086f1a8941226a356d8ea215944
Benjamin Carías <cariasbenjamin59@gmail.com>
2024-05-23 08:28:40 -0600

    Add Men/Women Carousel

--- src/components/HomeSectionCarousel/HomeSectionCarousel.jsx
+++ src/components/HomeSectionCarousel/HomeSectionCarousel.jsx
@@ -3,8 +3,11 @@ import 'react-alice-carousel/lib/alice-carousel.css';
 import { HomeSectionCard } from '../HomeSectionCard/HomeSectionCard';
 import { KeyboardArrowLeft } from '@mui/icons-material';
 import { Button } from '@mui/material';
+import { useState } from 'react';
 
-export const HomeSectionCarousel = () => {
+export const HomeSectionCarousel = ({ data, sectionName }) => {
+
+    const [activeIndex, setActiveIndex] = useState(0);
 
     const responsive = {
         0: { items: 1 },
@@ -12,34 +15,47 @@ export const HomeSectionCarousel = () => {
         1024: { items: 4 },
     };
 
-    const items = [1, 1, 1, 1, 1].map((item) => <HomeSectionCard />)
+    const items = data.slice(0, 10).map((item) => <HomeSectionCard product={item} />)
+
+    const slideNext = () => setActiveIndex(activeIndex + 1);
+    const slidePrev = () => setActiveIndex(activeIndex - 1);
+
+    const syncActiveIndex = ({ item }) => setActiveIndex(item);
 
     return (
         <div className=''>
-            <div className='relative p-2 pl-10'>
+            <h2 className='text-xl font-bold px-4 mb-6'>{sectionName}</h2>
+            <div className='relative p-2 pl-10 border border-gray-200'>
                 <AliceCarousel
                     items={items}
-                    autoPlayInterval={2000}
                     disableButtonsControls
                     disableDotsControls
                     responsive={responsive}
+                    onSlideChanged={syncActiveIndex}
+                    activeIndex={activeIndex}
                 />
-                <Button 
-                    variant='contained' 
-                    className='z-50' 
-                    sx={{position: 'absolute', top: '9rem', right: '0rem', transform:'translateX(50%) rotate(90deg)', bgcolor:'white'}}
-                    aria-label='next'
-                >
-                    <KeyboardArrowLeft sx={{transform:'rotate(90deg)', color: 'black'}}/>
-                </Button>
+                {activeIndex !== items.length-5 &&
+                    <Button
+                        variant='contained'
+                        className='z-50'
+                        sx={{ position: 'absolute', top: '9rem', right: '0rem', transform: 'translateX(50%) rotate(90deg)', bgcolor: 'white' }}
+                        aria-label='next'
+                        onClick={slideNext}
+                    >
+                        <KeyboardArrowLeft sx={{ transform: 'rotate(90deg)', color: 'black' }} />
+                    </Button>
+                }
 
-                <Button variant='contained' className='z-50' 
-                    sx={{position: 'absolute', top: '9rem', left: '0rem', transform:'translateX(-50%) rotate(90deg)', bgcolor:'white'}}
-                    aria-label='prev'
-                >
-                    <KeyboardArrowLeft sx={{transform:'rotate(-90deg)', color: 'black'}}/>
-                </Button>
+                {activeIndex !== 0 &&
+                    < Button variant='contained' className='z-50'
+                        sx={{ position: 'absolute', top: '9rem', left: '0rem', transform: 'translateX(-50%) rotate(90deg)', bgcolor: 'white' }}
+                        aria-label='prev'
+                        onClick={slidePrev}
+                    >
+                        <KeyboardArrowLeft sx={{ transform: 'rotate(-90deg)', color: 'black' }} />
+                    </Button>
+                }
             </div>
-        </div>
+        </div >
     )
 }
--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,4 +1,6 @@
-import { HomeSectionCarousel } from "../../components/HomeSectionCarousel/HomeSectionCarousel"
+import { HomeSectionCarousel } from "../../components/HomeSectionCarousel/HomeSectionCarousel";
+import { men_shirts } from "../../Data/Men/men_shirts.js";
+import { women_jeans } from "../../Data/Women/women_jeans.js";
 
 
 export const HomePage = () => {
@@ -6,14 +8,14 @@ export const HomePage = () => {
     <>
       <div className="relative hero-section">
         <div className="absolute top-1/2 left-10 lg:left-40 transform -translate-y-1/2 p-4">
-          <h1 className='text-5xl text-left lg:text-6xl font-bold max-w-44 lg:max-w-3xl italic leading-10'>Explore, Find, Enjoy: Your Virtual Marketplace</h1>
+          <h1 className='text-5xl text-left lg:text-5xl font-bold max-w-44 lg:max-w-3xl italic tracking-wide'>Explore, Find, Enjoy: Your Virtual Marketplace</h1>
           <button className="mt-4 font-semibold bg-indigo-600 text-white py-2 px-8 rounded">Start Exploring</button>
         </div>
       </div>
 
-      <div className="space-y-10 py-20 flex flex-col justify-center px-5 lg:px-10">
-        <HomeSectionCarousel/>
-        <HomeSectionCarousel/>
+      <div className="space-y-10 py-14 flex flex-col justify-center px-5 lg:px-10">
+        <HomeSectionCarousel data={men_shirts} sectionName={"Men's Shirts"}/>
+        <HomeSectionCarousel data={women_jeans} sectionName={"Women's Jeans"}/>
       </div>
 
     </>
